885fa724a83c035c1142308e0866aff82c5eddae
Erik Sharipov <erikwayne00@gmail.com>
2023-01-26 14:49:36 +0300

    1

--- src/components/section/GridEl.tsx
+++ src/components/section/GridEl.tsx
@@ -1,24 +1,11 @@
-import { motion } from 'framer-motion'
-
 export const GridEl = ({ text = '', img = '' }) => {
   return (
-    <motion.li
+    <li
       className='flex  justify-center items-center flex-col'
-      animate={{
-        scale: [1, 2, 2, 1, 1],
-        rotate: [0, 0, 180, 180, 0],
-        borderRadius: ['0%', '0%', '50%', '50%', '0%'],
-      }}
-      transition={{
-        duration: 2,
-        ease: 'easeInOut',
-        times: [0, 0.2, 0.5, 0.8, 1],
-        repeat: Infinity,
-        repeatDelay: 1,
-      }}
+      
     >
       <img className='h-4/6 bg-black rounded-2xl' src={img} alt='image' />
       <span className='font-bold pt-5'>{text}</span>
-    </motion.li>
+    </li>
   )
 }
